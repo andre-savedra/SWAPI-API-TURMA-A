09a255a83e03d6794dce8e0dc0afa0b95def31b5
andre-savedra <andresavedracruz@gmail.com>
2023-09-27 19:30:36 -0300

    adicionado exemplo com mysql

--- aula1/settings.py
+++ aula1/settings.py
@@ -85,12 +85,26 @@ WSGI_APPLICATION = 'aula1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#CONFIGURAÇÕES PARA SE COMUNICAR COM SQLITE!
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'testeTurmaA', #nome do BANCO
+        'HOST':'127.0.0.1', #endereço do BANCO
+        'USER':'fms_root', #usuario do banco
+        'PASSWORD':'fmsroot', #senha do banco
+        'PORT':'3306',
+    }   
 }
+
+
+
 
 
 # Password validation
